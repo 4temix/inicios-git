6eb01f2a94d769157758eab2495196da2c58de19
temixxgi <temixx@gmail.com>
2023-10-27 09:27:58 -0400

    prueba de condicionales

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 console.log("que hay")
 alert("hola buenas")
-alert("hola que tal")
 
 let arr = [100,20,200,300]
 
 for (const valor of arr) {
     console.log(valor+25)
+}
+
+
+let cont = 5,
+conta = 13;
+
+if(cont >= 1 && cont <= 12 && conta >= 0 && conta <= 12){
+    alert("hola que tal")
 }
