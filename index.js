58d1451827a82fa102fc76bfdda21b13666e0d85
temixxgi <temixx@gmail.com>
2023-11-01 21:31:18 -0400

    commit de spider-man

--- index.js
+++ index.js
@@ -10,14 +10,14 @@
 
 let cont = 8,
 conta = 0,
-mas = 0,
+mas = 1,
 more = 3
 
 // if(mas*conta*more === cont){
 //     alert("hola que tal desde el if")
 // }
 
-for (let index = 0; conta <= 500; index++) {
+for (let index = 1; conta <= 500; index++) {
     conta = cont*index
     console.log(`${mas} = ${conta}`)
     mas ++
